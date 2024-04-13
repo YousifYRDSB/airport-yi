5c733d4b0b8e07fcc057103e0e5ea79058fd3a25
isabellazhong <160078573+isabellazhong@users.noreply.github.com>
2024-04-13 19:58:04 -0400

    bonus 1  mergesort.ts

--- src/functions/mergesort.ts
+++ src/functions/mergesort.ts
@@ -165,6 +165,27 @@ export function catergorize<T>(arr: T[], main: T[], start: number, end:number):T
     return category; 
 }
 
+// bonus 1 
+function comapre(airOne: string, airTwo:string): number | undefined{
+    let indexOne = search(data.sortedID, airOne, data.airports.ID);
+    let indexTwo = search(data.sortedID, airTwo, data.airports.ID);
+
+    if(data.airports.type[indexOne] === data.airports.type[indexTwo]){
+      const lon1 = data.airports.longitude[indexOne];
+      const lon2 = data.airports.longitude[indexTwo];
+      const lat1 = data.airports.longitude[indexOne];
+      const lat2 = data.airports.longitude[indexTwo];
+      const dlon = lon2 - lon1;
+      const dlat = lat2 - lat1; 
+      const R = 6371; 
+
+      let d = 2*R*Math.asin(Math.sqrt((1-Math.cos(dlat)+ Math.cos(lat1)*Math.cos(lat2)*(1-Math.cos(dlon)))/2)); 
+
+      return d; 
+    }else{
+      return undefined; 
+    }
+}
 
 
 
