d28687fce10d496badc7aa1a6906f56d2714767e
isabellazhong <160078573+isabellazhong@users.noreply.github.com>
2024-04-13 18:12:29 -0400

    Update table.svelte

--- src/components/table.svelte
+++ src/components/table.svelte
@@ -5,12 +5,13 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { Table } from '@skeletonlabs/skeleton';
+	import {catergorize} from '../mergesort.ts'; 
 import type { TableSource } from '@skeletonlabs/skeleton';
 import { tableMapperValues } from '@skeletonlabs/skeleton';
 
 
 
-const sourceData = [
+let sourceData = [
 	{ position: 1, name: 'London', type: '1.0079', country: 'H', longitude:'4', value: 2.1, symbol: 'H' },
     { position: 2, name: 'Toronto', type: '4.0026', country: 'He', longitude:'4', value: 3.9, symbol: 'He' },
     { position: 3, name: 'Vancouver', type: '6.941', country: 'Li', longitude:'4', value: 4.2, symbol: 'Li' },
@@ -18,10 +19,11 @@ const sourceData = [
     { position: 5, name: 'Boron', type: '10.81', country: 'B', longitude:'4', value: 6.9, symbol: 'B' },
     { position: '', name: '', type: '', country: '', longitude:'', value: '', symbol: '' }
 
-];			
+];	
+
 const tableSimple: TableSource = {
 	// A list of heading labels.
-	head: ['Name', 'Id', 'Type', 'Iso_country', 'Iso_region', 'Longitude', 'Latitude'],
+	head: ['Name', 'Id', 'Type', 'Iso_country', 'Iso_region', 'Continent', 'Longitude', 'Latitude'],
 	// The data visibly shown in your table body UI.
 	body: tableMapperValues(sourceData, ['name', 'symbol', 'type', 'value', 'region', 'longitude', 'latitude']),
 	// Optional: The data returned when interactive is enabled and a row is clicked.
